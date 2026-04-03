78fe03642a6082fb3035492dcba3b37372b3aa11
nagendra376 <nagendraswsa@gmail.com>
2026-04-04 00:43:02 +0530

    solved 2nd max in array

---
+++ Arrays/2nd-max-in-array/index.js
@@ -0,0 +1,16 @@
+let arr = [10, 30 , 56, 43, 29, 64, 49, 60];
+
+let max = Math.max(arr[0], arr[1]);
+let smax= Math.min(arr[0], arr[1]);
+
+for(let i = 0 ; i<arr.length; i++){
+        if(arr[i]> max){
+            smax = max;
+            max = arr[i];
+        }else if( arr[i] > smax){
+            smax = arr[i];
+        }
+}
+
+console.log(max, smax)
+
