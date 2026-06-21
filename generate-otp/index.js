4e744df3c380e9af2b9c7f5a2a5db7c2ec40b2c6
nagendraiic <nagendra.dwevidi.dev.iic0115@gmail.com>
2026-06-22 01:25:10 +0530

    refactor: improve comments for OTP generation function

--- generate-otp/index.js
+++ generate-otp/index.js
@@ -1,3 +1,7 @@
+// o(n)
+
+// loop through n times and generate a random number between 0-9 and append it to the otp string
+
 function generateOtp(n) {
   let otp = "";
   for (let i = 0; i < n; i++) {
