2a49b4368f92e965778dace92e57f170ac015c42
nagendraiic <nagendra.dwevidi.dev.iic0115@gmail.com>
2026-07-05 13:54:01 +0530

    refactor: optimize bubble sort by adding early exit condition

--- Sorting/bubble-sort/index.js
+++ Sorting/bubble-sort/index.js
@@ -5,12 +5,17 @@ let arr = [64, 34, 25, 12, 22, 11, 90];
 function bubbleSort(arr) {
   // code here
   const n = arr.length;
+  let didSwap = 0;
 
   for (let i = n - 1; i >= 0; i--) {
     for (let j = 0; j <= i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        didSwap = 1;
       }
+    }
+    if (didSwap === 0) {
+      break;
     }
   }
   return arr;
